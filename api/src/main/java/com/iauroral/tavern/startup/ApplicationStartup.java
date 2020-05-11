8705b4f7f8e572bfcf91781ea5589d33f75fb70b
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-11 21:22:16 +0800

    start

--- api/src/main/java/com/iauroral/tavern/startup/ApplicationStartup.java
+++ api/src/main/java/com/iauroral/tavern/startup/ApplicationStartup.java
@@ -20,15 +20,36 @@ public class ApplicationStartup implements ApplicationListener<ContextRefreshedE
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         List<User> users = (List<User>) userRepository.findAll();
-        if (users.size() != 0) {
+        if (users.size() >= 4) {
             return;
         }
 
-        User user = new User();
-        user.setName("系统管理员");
-        user.setUsername("admin");
-        user.setPassword("admin");
-        user.setAuthority(User.ADMIN);
-        userRepository.save(user);
+        User admin = new User();
+        admin.setName("系统管理员");
+        admin.setUsername("admin");
+        admin.setPassword("admin");
+        admin.setAuthority(User.ADMIN);
+        userRepository.save(admin);
+
+        User manager = new User();
+        manager.setName("酒店经理");
+        manager.setUsername("manager");
+        manager.setPassword("manager");
+        manager.setAuthority(User.MANAGE);
+        userRepository.save(manager);
+
+        User front = new User();
+        front.setName("酒店前台");
+        front.setUsername("front");
+        front.setPassword("front");
+        front.setAuthority(User.FRONT);
+        userRepository.save(front);
+
+        User customer = new User();
+        customer.setName("顾客");
+        customer.setUsername("customer");
+        customer.setPassword("customer");
+        customer.setAuthority(User.CUSTOM);
+        userRepository.save(customer);
     }
 }
