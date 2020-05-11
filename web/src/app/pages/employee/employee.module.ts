4e861ff9e49276d3040249b7b7343c56de222834
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-12 07:30:43 +0800

    employee

--- web/src/app/pages/employee/employee.module.ts
+++ web/src/app/pages/employee/employee.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
 import { EmployeeRoutingModule } from './employee-routing.module';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, AddComponent, EditComponent],
   imports: [
     CommonModule,
     EmployeeRoutingModule
