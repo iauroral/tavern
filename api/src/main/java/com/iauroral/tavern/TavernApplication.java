bea2f61676a0658a62cbf6f88f52cc44ceff8e27
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-11 08:27:49 +0800

    init

---
+++ api/src/main/java/com/iauroral/tavern/TavernApplication.java
@@ -0,0 +1,13 @@
+package com.iauroral.tavern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavernApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/iauroral/tavern/TavernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iauroral.tavern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
