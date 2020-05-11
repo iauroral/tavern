7f7c8b0b2fc486498fa54bd0603fe6179067c4a7
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-11 21:13:39 +0800

    base-menu

--- web/src/app/layout/menu/menu.component.ts
+++ web/src/app/layout/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../service/user.service';
 import { User } from '../../entity/user';
+import { Menu } from '../../target/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,13 +11,26 @@ import { User } from '../../entity/user';
 })
 export class MenuComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
+  menus: Array<Menu> = new Array<Menu>();
+
+  constructor(private userService: UserService,
+              private router: Router) { }
 
   ngOnInit() {
     this.userService.me()
       .subscribe((user: User) => {
-        console.log(user);
+        if (user.authority === User.ADMIN) {
+          this.menus.push(new Menu('员工信息管理', 'employee', 'mdi mdi-home menu-icon'));
+          this.menus.push(new Menu('顾客信息管理', 'customer', 'mdi mdi-home menu-icon'));
+          this.menus.push(new Menu('客房信息管理', 'room', 'mdi mdi-home menu-icon'));
+          this.menus.push(new Menu('餐饮信息管理', 'catering', 'mdi mdi-home menu-icon'));
+          this.menus.push(new Menu('服务信息管理', 'service', 'mdi mdi-home menu-icon'));
+        }
       });
+  }
+
+  navigate(menu: Menu) {
+    this.router.navigateByUrl(menu.url);
   }
 
 }
---
+++ web/src/app/target/menu.ts
@@ -0,0 +1,14 @@
+export class Menu {
+
+  name: string;
+
+  url: string;
+
+  icon: string;
+
+  constructor(name: string, url: string, icon: string) {
+    this.name = name;
+    this.url = url;
+    this.icon = icon;
+  }
+}
