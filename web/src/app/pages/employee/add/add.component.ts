1dae1ba6208532afc08258d9fc8508b23b872713
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-12 12:19:34 +0800

    employee-add

--- web/src/app/pages/employee/add/add.component.ts
+++ web/src/app/pages/employee/add/add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
@@ -12,7 +13,8 @@ export class AddComponent implements OnInit {
   private addForm: FormGroup;
 
   constructor(private builder: FormBuilder,
-              private userService: UserService) { }
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit() {
     this.addForm = this.builder.group({
@@ -27,7 +29,8 @@ export class AddComponent implements OnInit {
   submit() {
     this.userService.save(this.addForm.value)
       .subscribe(() => {
-        console.log('success');
+        alert('添加成功');
+        this.router.navigateByUrl('employee');
       });
   }
 
