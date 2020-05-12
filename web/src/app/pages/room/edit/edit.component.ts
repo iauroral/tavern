0d0dd18fa2763fa293e00e33a71b14953d43496e
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-12 13:54:21 +0800

    edit

--- web/src/app/pages/room/edit/edit.component.ts
+++ web/src/app/pages/room/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RoomService } from '../../../service/room.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../../../entity/user';
+import { Room } from '../../../entity/room';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  private id: number;
+
+  constructor(private builder: FormBuilder,
+              private roomService: RoomService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      number: ['', Validators.required],
+      type: [0, Validators.required],
+      description: ['', Validators.required],
+      price: [0, Validators.required]
+    });
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.roomService.getRoomById(this.id)
+      .subscribe((room: Room) => {
+        this.form.patchValue({
+          number: room.number,
+          type: room.type,
+          description: room.description,
+          price: room.price
+        });
+      });
+  }
+
+  submit() {
+    this.roomService.update(this.id, this.form.value)
+      .subscribe(() => {
+        alert('更新成功');
+        this.router.navigateByUrl('room');
+      });
   }
 
 }
