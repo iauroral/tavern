781fbf2cc8544c9b4333e192b190a5eae5666b57
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-12 14:00:54 +0800

    main

--- web/src/app/pages/room/main/main.component.ts
+++ web/src/app/pages/room/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../../../entity/room';
+import { RoomService } from '../../../service/room.service';
+import { User } from '../../../entity/user';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  rooms: Array<Room> = new Array<Room>();
+
+  constructor(private roomService: RoomService) { }
 
   ngOnInit() {
+    this.load();
   }
 
+  load() {
+    this.roomService.getAll()
+      .subscribe((data: any) => {
+        this.rooms = data._embedded.rooms;
+      });
+  }
+
+  delete(room: Room) {
+    this.roomService.delete(room.id)
+      .subscribe(() => {
+        alert('删除成功');
+        this.load();
+      });
+  }
 }
