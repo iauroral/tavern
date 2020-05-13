f376e453e970f838b8ab368a190d2251111d582b
张喜硕 <zhangxishuo1998@outlook.com>
2020-05-13 08:07:44 +0800

    catering

--- web/src/app/pages/catering/edit/edit.component.ts
+++ web/src/app/pages/catering/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CateringService } from '../../../service/catering.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Room } from '../../../entity/room';
+import { Catering } from '../../../entity/catering';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  private id: number;
+
+  constructor(private builder: FormBuilder,
+              private cateringService: CateringService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      name: ['', Validators.required],
+      price: [0, Validators.required]
+    });
+    this.id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+    this.cateringService.getCateringById(this.id)
+      .subscribe((catering: Catering) => {
+        this.form.patchValue({
+          name: catering.name,
+          price: catering.price
+        });
+      });
+  }
+
+  submit() {
+    this.cateringService.update(this.id, this.form.value)
+      .subscribe(() => {
+        alert('更新成功');
+        this.router.navigateByUrl('catering');
+      });
   }
 
 }
